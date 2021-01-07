a573af5ae2487320791427b7b82e3dba04805083
blue <michael.hao25@gmail.com>
2021-01-07 23:50:47 +0800

    修复问题

--- script/api.js
+++ script/api.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2014 apicloud.com
  */
 
-
 // let script = document.createElement('script');
 // script.src = "../web_adapter/script/adapter.js";
 // document.body.appendChild(script);
